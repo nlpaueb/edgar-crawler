8af3b37831c75880c9b01c3c78af773bc7ee9875
Lefteris Loukas <eleftheriosloukas@gmail.com>
2021-10-12 13:54:27 +0300

    Create folders for datasets and logs on startup

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
 import os
 DATASET_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'datasets')
 LOGGING_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logs')
+
+if not os.path.exists(DATASET_DIR):
+	os.mkdir(DATASET_DIR)
+
+if not os.path.exists(LOGGING_DIR):
+	os.mkdir(LOGGING_DIR)
